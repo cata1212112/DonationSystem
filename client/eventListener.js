76f58af28d495e1e577bed071a1488d481a94560
Stan Catalin <48752183+cata1212112@users.noreply.github.com>
2024-05-16 23:04:51 +0300

    add gas estimation & ipfs image storing

--- client/eventListener.js
+++ client/eventListener.js
@@ -1,4 +1,4 @@
-const {Web3} = require('web3');
+const {Web3} = require('web3');  // Ensure Web3 is correctly imported
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
@@ -7,34 +7,32 @@ const port = 3000;
 
 app.use(cors());
 
+// Initialize Web3 with an HTTP provider
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'));
-const doneeFactoryAddress = '0x0afF9ed7941A6BAD7F878Cbf34A49182f1A85A2f';
-const doneeFactoryABI = require('./src/contracts/DoneeFactory.json').abi;
+
+// Check if Web3 is connected
+web3.eth.net.isListening()
+    .then(() => console.log('Web3 is connected'))
+    .catch(e => console.error('Something went wrong', e));
+
+// Read the ABI and contract address from the local JSON file
+const jsonData = JSON.parse(fs.readFileSync('./src/contracts/DoneeFactory.json', 'utf8'));
+const networkId = Object.keys(jsonData.networks)[0];
+const doneeFactoryAddress = jsonData.networks[networkId].address;
+const doneeFactoryABI = jsonData.abi;
+console.log(doneeFactoryAddress)
+
+// Create the contract instance
 const doneeFactoryContract = new web3.eth.Contract(doneeFactoryABI, doneeFactoryAddress);
 
+// console.log(doneeFactoryContract.events);
 
-function listenForEvents() {
-    doneeFactoryContract.events.DoneeCreated({}, (error, event) => {
-        if (error) {
-            console.error('Error listening to DoneeCreated events:', error);
-            return;
-        }
-        console.log('DoneeCreated event:', event.returnValues);
-        storeEvent(event);
-    });
-
-    doneeFactoryContract.events.allEvents({}, (error, event) => {
-        if (error) {
-            console.error('Error listening to events:', error);
-            return;
-        }
-        console.log('Event:', event.event, event.returnValues);
-        storeEvent(event);
-    });
-}
-
+doneeFactoryContract.events.DoneeCreated(function(error, event){ console.log(event);
+storeEvent(event)
+})
 
 function storeEvent(event) {
+    console.log("intrat event")
     const eventLog = {
         event: event.event,
         returnValues: event.returnValues,
@@ -42,7 +40,6 @@ function storeEvent(event) {
     };
     fs.appendFileSync('events.log', JSON.stringify(eventLog) + '\n', 'utf8');
 }
-
 
 app.get('/events', (req, res) => {
     fs.readFile('events.log', 'utf8', (err, data) => {
@@ -57,5 +54,4 @@ app.get('/events', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Event listener server running at http://localhost:${port}`);
-    listenForEvents();
 });
